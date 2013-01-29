b6185e33031157124e89bb226eb266a313eaea34
Christi Erba <cerba@u.rochester.edu>
2013-01-28 22:10:23 -0600

    Fill Histo Problem still not fixed

--- test.py
+++ test.py
@@ -1,6 +1,10 @@
+# Test Document for week1.py
+
 import sys
 import ROOT as r
 r.gROOT.SetBatch(1)
+r.gInterpreter.GenerateDictionary('vector<ROOT::Math::LorentzVector<ROOT::Math::PtEtaPhiM4D<Double32_t> > >','vector;Math/LorentzVector.h')
+
 
 # Define a File Name for Histogram
 if len(sys.argv)<2 :
@@ -9,37 +13,36 @@ if len(sys.argv)<2 :
     
 fileName = sys.argv[1]
 
+
 # Open File
 f = r.TFile.Open("root://xrootd.grid.hep.ph.ic.ac.uk///store/user/bbetchar/TOP/automated/2013_01_15_05_13_16/TT_CT10_TuneZ2star_8TeV-powheg-tauola.Summer12_DR53X-PU_S10_START53_V7A-v1.AODSIM/topTuple_21_1_q59.root")
 
 tree = f.Get('topRef/tree')
 
+
 # Make Canvas, Set Legend
 canvas = r.TCanvas()
-canvas.Divide(2,2)
+#canvas.Divide(2,2)
 
 leg = r.TLegend(0.7,0.7,1,1)
 leg.SetHeader = ("Plot Type")
 
+
 # Define Histogram
-gluP4 = r.TH1D('Gluon Transverse Momentum','',10,0,10)
+gluplot = r.TH1D('Gluon Transverse Momentum','',100,0,100)
 
 
 # Fill Histogram
 
-#for i in tree.Row :
-if tree.genPdgId == 21 :
-    gluP4.Fill(genP4.pt())
-
-
+for iEvent in range(tree.GetEntries()) :
+    tree.GetEntry(iEvent)
+    gluplot.Fill(tree.genP4.pt())
 
 # Plot Histogram
-canvas.cd(1)
-gluP4.Draw()
+
+#canvas.cd(1)
+gluplot.Draw()
 canvas.Print('%s.pdf'%fileName)
-
-
-
 
 
 #End Program
