64d34db2d6211e26e2cf1a74fc8e8735e11a5117
Christi Erba <cerba@u.rochester.edu>
2013-02-11 11:26:27 -0600

    updated open angle plots to include initial gluons; QW plots need errorchecking; also including corresponding .pdf file

--- modangplot.py
+++ modangplot.py
@@ -11,13 +11,17 @@ f = r.TFile.Open("root://xrootd.grid.hep.ph.ic.ac.uk///store/user/bbetchar/TOP/a
 tree = f.Get('topRef/tree')
 
 # Define Histogram
-gluplot = r.TH1D('Gluon Transverse Momentum','g,p_T',100,0,100)
-bplot = r.TH1D('Bottom Transverse Momentum','b;p_t',100,0,100)
-abplot = r.TH1D('Anti-Bottom Transverse Momentum','#bar{b};p_T',100,0,100)
-tplot = r.TH1D('Top Transverse Momentum','t;p_t',100,0,100)
-atplot = r.TH1D('Anti-Top Transverse Momentum','#bar{t};p_T',100,0,100)
-qwplot = r.TH1D('Q from W+ Transverse Momentum','',100,0,100)
-aqwplot = r.TH1D('Q from W- Transverse Momentum','',100,0,100)
+gluplot = r.TH1D('Gluon Transverse Momentum','g,p_T',100,0,450)
+bplot = r.TH1D('Bottom Transverse Momentum','b;p_t',100,0,450)
+abplot = r.TH1D('Anti-Bottom Transverse Momentum','#bar{b};p_T',100,0,450)
+tplot = r.TH1D('Top Transverse Momentum','t;p_t',100,0,450)
+atplot = r.TH1D('Anti-Top Transverse Momentum','#bar{t};p_T',100,0,450)
+qwplot = r.TH1D('Q from W+ Transverse Momentum','',100,0,450)
+aqwplot = r.TH1D('Q from W- Transverse Momentum','',100,0,450)
+igluplot = r.TH1D('Gluon First P Transverse Momentum','g,p_T',100,0,450)
+ingluplot = r.TH1D('Gluon Second P Transverse Momentum','g,p_T',100,0,450)
+
+
 
 glubAngl = r.TH1D('Open Angle Gluon vs. B','',100,-1,1)
 gluBbarAngl = r.TH1D('Open Angle Gluon vs. Bbar','',100,-1,1)
@@ -25,6 +29,9 @@ glutAngl = r.TH1D('Open Angle Gluon vs. t','',100,-1,1)
 gluTbarAngl = r.TH1D('Open Angle Gluon vs. tbar','',100,-1,1)
 gluQwAngl = r.TH1D('Open Angle Gluon vs. QW+','',100,-1,1)
 gluAqwAngl = r.TH1D('Open Angle Gluon vs. QW-','',100,-1,1)
+gluipAngl = r.TH1D('Open Angle Gluon vs. P1','',100,-1,1)
+gluinpAngl = r.TH1D('Open Angle Gluon vs. P2','',100,-1,1)
+
 
 
 # Fill Histogram
@@ -37,10 +44,12 @@ for j in range(tree.GetEntries()) :
     ab = next(( i for i in index if tree.genPdgId[i] == -5 and tree.genMotherIndex[i] == 7), None)
     t = next((i for i in index if tree.genPdgId[i] == 6), None)
     at = next((i for i in index if tree.genPdgId[i] == -6), None)
-    qw = next((i for i in index if tree.genMotherIndex[i] == 24 and tree.genPdgId[i] == 1 or tree.genPdgId[i] == 2 or tree.genPdgId[i] == 3 or tree.genPdgId[i] == 4 or tree.genPdgId[i] == 5 or tree.genPdgId[i] == 6), None)          
-    aqw = next((i for i in index if tree.genMotherIndex[i] == -24 and tree.genPdgId[i] == -1 or tree.genPdgId[i] == -2 or tree.genPdgId[i] == -3 or tree.genPdgId[i] == -4 or tree.genPdgId[i] == -5 or tree.genPdgId[i] == -6), None)          
-
-    if glu!=None :
+    qw = next((i for i in index if tree.genMotherPdgId[i] == 24 and tree.genPdgId[i] == 1 or tree.genPdgId[i] == 2 or tree.genPdgId[i] == 3 or tree.genPdgId[i] == 4 or tree.genPdgId[i] == 5 or tree.genPdgId[i] == 6), None)          
+    aqw = next((i for i in index if tree.genMotherPdgId[i] == -24 and tree.genPdgId[i] == -1 or tree.genPdgId[i] == -2 or tree.genPdgId[i] == -3 or tree.genPdgId[i] == -4 or tree.genPdgId[i] == -5 or tree.genPdgId[i] == -6), None)          
+    iglu = next((i for i in index if tree.genPdgId[i] == 21 and tree.genMotherIndex[i] == 0),None)
+    inglu = next((i for i in index if tree.genPdgId[i] == 21 and tree.genMotherIndex[i] == 1),None)
+    
+    if glu!=None and iglu!=None and inglu!=None :
         gluplot.Fill(tree.genP4[glu].pt())
         glubAngl.Fill(r.Math.VectorUtil.CosTheta(tree.genP4[glu],tree.genP4[b]))
         gluBbarAngl.Fill(r.Math.VectorUtil.CosTheta(tree.genP4[glu],tree.genP4[ab]))
@@ -48,6 +57,9 @@ for j in range(tree.GetEntries()) :
         gluTbarAngl.Fill(r.Math.VectorUtil.CosTheta(tree.genP4[glu],tree.genP4[at]))
         gluQwAngl.Fill(r.Math.VectorUtil.CosTheta(tree.genP4[glu],tree.genP4[qw]))
         gluAqwAngl.Fill(r.Math.VectorUtil.CosTheta(tree.genP4[glu],tree.genP4[aqw]))
+        gluipAngl.Fill(r.Math.VectorUtil.CosTheta(tree.genP4[glu],tree.genP4[iglu]))
+        gluinpAngl.Fill(r.Math.VectorUtil.CosTheta(tree.genP4[glu],tree.genP4[inglu]))
+        
 
     bplot.Fill(tree.genP4[b].pt())
     abplot.Fill(tree.genP4[ab].pt())
@@ -67,7 +79,7 @@ leg.SetHeader = ("Plot Type")
 
 canvas.Print(fileName + '[') # open for multipage printing
 
-for hist in [gluplot, bplot, abplot, tplot, atplot, qwplot, aqwplot, glubAngl, gluBbarAngl, glutAngl, gluTbarAngl, gluQwAngl, gluAqwAngl] :
+for hist in [gluplot, bplot, abplot, tplot, atplot, qwplot, aqwplot, glubAngl, gluBbarAngl, glutAngl, gluTbarAngl, gluQwAngl, gluAqwAngl, gluipAngl, gluinpAngl] :
     hist.Draw()
     canvas.Print(fileName)
 canvas.Print(fileName + ']') # close for multipage printing
