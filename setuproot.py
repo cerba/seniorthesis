9f22a085d3c9f8c9791cee654d6d64ee3403678d
Burton Betchart <betchart@gmail.com>
2013-02-10 18:22:59 -0600

    fix setuproot

--- setuproot.py
+++ setuproot.py
@@ -4,20 +4,7 @@ import ROOT as r
 
 def cppROOTDictionariesToGenerate() :
     return [
-        ("pair<string,bool>", "string"),
-        ("map<std::string,bool>", "string;map"),
-        ("pair<string,string>", "string"),
-        ("map<std::string,string>", "string;map"),
-        ("ROOT::Math::Cartesian3D<float>", "Math/Point3D.h"),
-        ("ROOT::Math::DisplacementVector3D<ROOT::Math::Cartesian3D<float>,ROOT::Math::DefaultCoordinateSystemTag>", "Math/Vector3D.h"),
-        ("vector<ROOT::Math::DisplacementVector3D<ROOT::Math::Cartesian3D<float>,ROOT::Math::DefaultCoordinateSystemTag> >", "vector;Math/Vector3D.h"),
-        ("ROOT::Math::PositionVector3D<ROOT::Math::Cartesian3D<float>,ROOT::Math::DefaultCoordinateSystemTag>", "Math/Point3D.h"),
-        ("vector<ROOT::Math::PositionVector3D<ROOT::Math::Cartesian3D<float>,ROOT::Math::DefaultCoordinateSystemTag> >", "vector;Math/Point3D.h"),
-        #ROOT::Math::LorentzVector<ROOT::Math::PtEtaPhiM4D<float> > etc. is addressed in linkdef.cxx
-        ("vector<ROOT::Math::LorentzVector<ROOT::Math::PtEtaPhiM4D<float> > >", "vector;Math/LorentzVector.h"),
-        ("vector< vector< float > >", "vector"),
-        ("vector< vector< unsigned int> >", "vector"),
-        ("vector< vector< int> >", "vector"),
+        ('vector<ROOT::Math::LorentzVector<ROOT::Math::PtEtaPhiM4D<Double32_t> > >','vector;Math/LorentzVector.h')
         ]
 
 def generateDictionaries(inList, dir = None) :
@@ -40,5 +27,5 @@ def initializeROOT(r, cppFiles = []) :
         
 generateDictionaries(cppROOTDictionariesToGenerate())
         
-initializeROOT(r,["linkdef.cxx"])
+initializeROOT(r,["cpp/linkdef.cxx"])
         
